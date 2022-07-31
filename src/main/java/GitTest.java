9fcd3219961d7fc8984c2a3d60857f33b5cfa5b5
龚心泽 <gongxinze@ningzefintech.com>
2022-07-31 22:10:06 +0800

    branch01 first-commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,7 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         String str1="Hello";
-        String str2="World";
+        String str2="World!";
         String result=str1+str2;
         System.out.println(result);
     }
